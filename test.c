f8a7cd55c69cb6f788d8fb66f689a95957bccd52
Mihawk Hu <mihawkhu@gmail.com>
2016-04-13 10:50:45 +0800

    Update test.c

--- test.c
+++ test.c
@@ -6,12 +6,12 @@
 
 int main(int argc, char *argv[]) {
 	pid_t parent_pid = getpid();	//get current process pid
-	printf("5140519019 Parent, %d\n", parent_pid);
-
 	pid_t child_pid = fork();	//get child process pid
-	printf("5140519019 Child, %d\n", child_pid);
-
-	execl("./ptree", "ptree", (char*)0);	// execute ptree in child process
-
+	
+	if (child_pid == 0) {
+		printf("5140519019 Parent, %d\n", parent_pid);	
+		printf("5140519019 Child, %d\n", child_pid);	
+		execl("./ptree", "ptree", (char*)0);	// execute ptree in child process	
+	}
 	return 0;
 }
